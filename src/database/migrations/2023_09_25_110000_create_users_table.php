a4322afca6f625de6b816fab21df0ebec75499bc
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2023-10-24 11:13:14 +0300

    FIx crashes and errors in production - Use predis library for interfacing with redis,   instead of phpredis library - Fix create_users_table migration

--- src/database/migrations/2023_09_25_110000_create_users_table.php
+++ src/database/migrations/2023_09_25_110000_create_users_table.php
@@ -1,10 +1,11 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -13,7 +14,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->tinyText('name');
-            $table->tinyText('email')->unique();
+            $table->string('email')->unique();
             $table->tinyText('password');
             $table->timestamps();
         });
